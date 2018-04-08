8a7458b4b5ed90c79610b209a1931b7d8c2e3f02
David Perez <david.perez@us.abb.com>
2018-04-08 15:07:39 -0400

    389. Find the Difference fixed

--- mock.js
+++ mock.js
@@ -6,17 +6,9 @@
 
  //389. Find the Difference
 var findTheDifference = function (s, t) {
-
-    var i = 0;
-    var j = 0;
-    var result = "";
-
-    while (j < t.length) {
-        if (s[i] != t[j] || i == s.length)
-            result += t[j];
-        else
-            i++;
-        j++;
-    }
-    return result;
+    if (s.length === 0) return t;
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var sSum = s.split('').map(e => e.charCodeAt(0) - 'a'.charCodeAt(0)).reduce((a, b) => a + b);
+    var tSum = t.split('').map(e => e.charCodeAt(0) - 'a'.charCodeAt(0)).reduce((a, b) => a + b);
+    return letters[tSum - sSum];
 };
