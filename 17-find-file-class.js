b8241c375418d1c271115c70e30729ab4ee4fc34
David Perez <kitsunebrave@gmail.com>
2018-11-16 16:28:46 -0500

    find file class observable obj

---
+++ 17-find-file-class.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// dependencies
+
+const EventEmitter = require('events').EventEmitter;
+const path = require('path');
+
+// definition
+
+class FindFiles extends EventEmitter {
+
+  constructor(extension) {
+    super()
+    this.extension = extension
+    this.files = []
+  }
+
+  addFiles(file) {
+    this.files.push(file);
+    return this;
+  }
+
+  // check for matches
+
+  findFiles() {
+    process.nextTick(() => {
+      this.files.forEach(file => {
+        if(path.extname(file) === this.extension) {
+          this.emit('match', file);
+        }
+      })
+    })
+    return this;
+  }
+}
+
+// instantiation of observable object
+const FindFileSearcher = new FindFiles('.js');
+
+// ImplementationFindFilesSearcher
+  .addFile('file1.js')
+  .addFile('file2.md')
+  .addFile('file3.js')
+  .findFiles()
+  .on('match', console.log)
+  .on('error', err => {
+    return console.error(err)
+  })
