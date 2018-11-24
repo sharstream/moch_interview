3c4cbec8d640bb82831ab5fb9433d373559905bb
David Perez <kitsunebrave@gmail.com>
2018-11-24 11:28:49 -0500

    Euler Problem 4 largest palindrome product

---
+++ 28-euler-problem-4-largest-palindrome-product.js
@@ -0,0 +1,35 @@
+// Palindrome number reads the same both way. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+const largestPalindromeProduct = digit => {
+
+  const palindrome = num => {
+    return parseInt(num.toString().split('').reverse().join('')) === num;
+  }
+
+  var start = Math.pow(10, digit - 1),
+      end = Math.pow(10, digit);
+
+  var x, y, product, largest = 0,
+  productX, productY;
+  for (x = start; x < end; x++) {
+    for (y = start; y < end; y++) {
+      
+      product = x * y;
+
+      if (palindrome(product) && largest < product) {
+        // if (product > largest) {
+        largest = product;
+        productX = x;
+        productY = y;
+        console.log(`largest products between ${productX} * ${productY} is`, product);
+        // }
+      }
+    }
+  }
+
+  return largest;
+
+}
+
+console.log(`largest palindrom two 3-digit is: `, largestPalindromeProduct(3));
