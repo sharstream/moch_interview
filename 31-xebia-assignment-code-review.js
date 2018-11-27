93f3e874da0a657d61a8c17377aefa015c63b9dc
David Perez <kitsunebrave@gmail.com>
2018-11-27 16:09:03 -0500

    xebi code section

---
+++ 31-xebia-assignment-code-review.js
@@ -0,0 +1,30 @@
+
+const addKeys = () => {
+  var employee = [{
+      id: 123,
+      name: 'David'
+    },
+    {
+      id: 456,
+      name: 'Mayank'
+    }
+  ]
+  // //adding new property name
+
+  const addName = name => {
+    return name
+  }
+  employee.map(currentValue => {
+    return currentValue.name === 'David' 
+      ? currentValue.displayName = addName('David') 
+      : currentValue.displayName = addName('Mayank')
+  })
+
+  employee.forEach(element => {
+    console.log(`{ \n id: ${element.id}, \n name: ${element.name}, \n displayName: ${element.displayName} \n}`);
+  })
+}
+
+// const addPropertyName = ar => ar.map(property )
+
+addKeys();
