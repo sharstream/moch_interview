16e60829e7b917e1b3b4feef546901917fa9ab98
David Perez <kitsunebrave@gmail.com>
2018-11-29 20:18:12 -0500

    building todolist in react simple example

---
+++ 32-build-todolist-react-component.js
@@ -0,0 +1,35 @@
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(itemText) {
+      return React.DOM.li(null, itemText);
+    };
+    return React.DOM.lu(null, this.props.items.map(createItem));
+  }
+});
+var TodoApp = React.createClass({
+  getInitialState : function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function() {
+    return (
+      React.DOM.div(null,
+        React.DOM.h3(null, "TODO"),
+        TodoList( {items: this.state.items} ),
+        React.DOM.form( {onSubmit:this.handleSubmit},
+          React.DOM.input( {onChange:this.state.onChange, value:this.state.text} ),
+          React.DOM.button(null, 'Add #' + (this.state.items.length + 1))
+        )
+      )
+    );
+  }
+});
+React.renderComponent(TodoApp(null), mountNode);
