3d2a907bc6f4b62ac5d1af107dcf08df06a354b2
David Perez <kitsunebrave@gmail.com>
2018-12-05 18:49:58 -0500

    compisition over inheritance

--- 34-object-prototype-class-inheritance.js
+++ 34-object-prototype-class-inheritance.js
@@ -146,3 +146,31 @@ function instanceOf(object, constructor) {
 var dennis = new Engineer('Doe, Dennis', ['collabra'], 'hugo');
 // doesn't inherit this new property
 Hobbyist.prototype.equipment = ['mask', 'fins', 'regulator', 'bcd'];
+
+// Composition over Inheritance
+
+const barker = state => {
+  bark: () => console.log('Woof, I am ' + state.name)
+}
+const driver = state => {
+  drive: () => state.position = state.position + state.speed
+}
+const killer = state => {
+  kill: () => console.log('I will kill ' + state.name)
+}
+barker({name: 'karo'}).bark()
+
+ const murderRobotDog = name => {
+   let state = {
+     name,
+     speed: 100,
+     position: 0
+   }
+   return Object.assign(
+     {},
+     barker(state),
+     driver(state),
+     killer(state)
+   )
+ }
+ murderRobotDog('sniffles').bark()
